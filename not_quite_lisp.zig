7c1cce0e7f7b8d1316d410b9cad79611bde3ac6c
Bojan Čoka <coka@fastmail.com>
2025-04-23 01:32:07 +0200

    --- Part Two ---

--- not_quite_lisp.zig
+++ not_quite_lisp.zig
@@ -1,19 +1,70 @@
 const std = @import("std");
+const testing = std.testing;
 
-pub fn main() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
-    const input = try std.fs.cwd().readFileAlloc(allocator, "input.txt", 1024 * 1024);
-    defer allocator.free(input);
-    var iterator = std.unicode.Utf8Iterator{ .bytes = input, .i = 0 };
+fn follow(directions: []const u8) i32 {
     var floor: i32 = 0;
-    while (iterator.nextCodepoint()) |c| {
+    for (directions) |c| {
         if (c == '(') {
             floor += 1;
         } else if (c == ')') {
             floor -= 1;
         }
     }
-    std.debug.print("{d}\n", .{floor});
+    return floor;
+}
+
+test follow {
+    try testing.expectEqual(0, follow("(())"));
+    try testing.expectEqual(0, follow("()()"));
+    try testing.expectEqual(3, follow("((("));
+    try testing.expectEqual(3, follow("(()(()("));
+    try testing.expectEqual(3, follow("))((((("));
+    try testing.expectEqual(-1, follow("())"));
+    try testing.expectEqual(-1, follow("))("));
+    try testing.expectEqual(-3, follow(")))"));
+    try testing.expectEqual(-3, follow(")())())"));
+}
+
+fn computeBasementEntryPosition(directions: []const u8) u32 {
+    var position: u32 = 0;
+    var floor: i32 = 0;
+    for (directions) |c| {
+        position += 1;
+        if (c == '(') {
+            floor += 1;
+        } else if (c == ')') {
+            floor -= 1;
+        }
+        if (floor < 0) {
+            break;
+        }
+    }
+    return position;
+}
+
+test computeBasementEntryPosition {
+    try testing.expectEqual(1, computeBasementEntryPosition(")"));
+    try testing.expectEqual(5, computeBasementEntryPosition("()())"));
+}
+
+fn readInput(allocator: std.mem.Allocator) ![]u8 {
+    return try std.fs.cwd().readFileAlloc(
+        allocator,
+        "input.txt",
+        1024 * 1024,
+    );
+}
+
+test "Part One" {
+    const directions = try readInput(testing.allocator);
+    defer testing.allocator.free(directions);
+    const answer = follow(directions);
+    try testing.expectEqual(answer, 138);
+}
+
+test "Part Two" {
+    const directions = try readInput(testing.allocator);
+    defer testing.allocator.free(directions);
+    const answer = computeBasementEntryPosition(directions);
+    try std.testing.expectEqual(answer, 1771);
 }
